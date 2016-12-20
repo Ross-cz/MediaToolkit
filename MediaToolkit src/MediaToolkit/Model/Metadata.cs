fec3ed2f908f6cb3186f775e30a2bbdce676cb84
Rostislav Hejduk <rostislav.hejduk@prozeta.eu>
2016-12-20 16:49:42 +0100

    Parse width and height

--- MediaToolkit src/MediaToolkit/Model/Metadata.cs
+++ MediaToolkit src/MediaToolkit/Model/Metadata.cs
@@ -2,31 +2,33 @@
 
 namespace MediaToolkit.Model
 {
-    public class Metadata
-    {
-        internal Metadata() { }
-        public TimeSpan Duration { get; internal set; }
-        public Video VideoData { get; internal set ; }
-        public Audio AudioData { get; internal set; }
+	public class Metadata
+	{
+		internal Metadata() { }
+		public TimeSpan Duration { get; internal set; }
+		public Video VideoData { get; internal set; }
+		public Audio AudioData { get; internal set; }
 
-        public class Video
-        {
-            internal Video() { }
-            public string Format { get; internal set; }
-            public string ColorModel { get; internal set; }
-            public string FrameSize { get; internal set; }
-            public int? BitRateKbs { get; internal set; }
-            public double Fps { get; internal set; }
-        }
+		public class Video
+		{
+			internal Video() { }
+			public string Format { get; internal set; }
+			public string ColorModel { get; internal set; }
+			public string FrameSize { get; internal set; }
+			public int Width { get; internal set; }
+			public int Height { get; internal set; }
+			public int? BitRateKbs { get; internal set; }
+			public double Fps { get; internal set; }
+		}
 
-        public class Audio 
-        {
-            internal Audio() { }
+		public class Audio
+		{
+			internal Audio() { }
 
-            public string Format { get; internal set; }
-            public string SampleRate { get; internal set; }
-            public string ChannelOutput { get; internal set; }
-            public int BitRateKbs { get; internal set; }
-        }
-    }
+			public string Format { get; internal set; }
+			public string SampleRate { get; internal set; }
+			public string ChannelOutput { get; internal set; }
+			public int BitRateKbs { get; internal set; }
+		}
+	}
 }
